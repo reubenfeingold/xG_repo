2fc15d1755580b0d8aefcd8e854b6e0a854d09cd
reubenfeingold <reubenfeingold@gmail.com>
2026-01-16 21:58:13 -0500

    foo

--- play_by_play.py
+++ play_by_play.py
@@ -3,7 +3,7 @@ from nhlpy import NHLClient
 client = NHLClient()
 
 #Get all the play by data which is a dict and we only want to focus on the key "plays"
-play_by_play = client.game_center.play_by_play(game_id="2023020280")
+play_by_play = client.game_center.play_by_play(game_id="2025020737")
 
 #normalize plays column so that you only have a df of plays which was a nested dict
 plays_df = pd.json_normalize(play_by_play["plays"])
@@ -97,42 +97,64 @@ boxscore["Faceoff Percentage"] = boxscore["Faceoff Wins"] / f_taken.replace(0, p
 #print(boxscore)
 
 
+pid_list = boxscore["playerId"].tolist()
+player_names = {}
+player_teams = {}
 
-box = client.game_center.boxscore(game_id="2023020280")
-away_id = box["awayTeam"]["id"]
-home_id = box["homeTeam"]["id"]
+for pid in pid_list:
+    career_stats = client.stats.player_career_stats(player_id=int(pid))
+    first_name = career_stats["firstName"]["default"]
+    last_name = career_stats["lastName"]["default"]
+    full_name = f"{first_name} {last_name}"
+    player_names[pid] = full_name
+    
+    # Get current team abbreviation
+    team_abbrev = career_stats["currentTeamAbbrev"]
+    player_teams[pid] = team_abbrev
 
+# Add player names and teams to boxscore
+boxscore['Name'] = boxscore['playerId'].map(player_names)
+boxscore['Team'] = boxscore['playerId'].map(player_teams)
 
+boxscore.insert(0, 'Name', boxscore.pop('Name'))
+boxscore.insert(1, 'Team', boxscore.pop('Team'))
 
-away_abbr = box["awayTeam"]["abbrev"]
-home_abbr = box["homeTeam"]["abbrev"]
-print(away_abbr, home_abbr)
-
-away_roster = client.teams.team_roster(team_abbr=away_abbr, season="20232024")
-home_roster = client.teams.team_roster(team_abbr=home_abbr, season="20232024")
-
-away_roster = client.teams.team_roster(team_abbr=away_abbr, season="20232024")
-home_roster = client.teams.team_roster(team_abbr=home_abbr, season="20232024")
-
-# Create away players dataframe
-away_players = pd.DataFrame(away_roster['forwards'] + away_roster['defensemen'] + away_roster['goalies'])
-away_players['playerId'] = away_players['id']
-away_players['playerName'] = away_players['firstName'].apply(lambda x: x['default']) + " " + away_players['lastName'].apply(lambda x: x['default'])
-away_players['team'] = away_abbr
-away_players = away_players[['playerId', 'playerName', 'team']]
-
-# Create home players dataframe
-home_players = pd.DataFrame(home_roster['forwards'] + home_roster['defensemen'] + home_roster['goalies'])
-home_players['playerId'] = home_players['id']
-home_players['playerName'] = home_players['firstName'].apply(lambda x: x['default']) + " " + home_players['lastName'].apply(lambda x: x['default'])
-home_players['team'] = home_abbr
-home_players = home_players[['playerId', 'playerName', 'team']]
-
-# Combine both rosters
-all_players = pd.concat([away_players, home_players], ignore_index=True)
-
-# Merge boxscore with player info
-boxscore = all_players.merge(boxscore, on="playerId", how="left")
-boxscore = boxscore.fillna(0)
-
+boxscore = boxscore.sort_values(by='Team', ascending=False).reset_index(drop=True)
 boxscore.to_csv('boxscore.csv', index=False)
+#Below is another way to add player names and teams but it caused me problems and alos uses the boxscore.
+
+# box = client.game_center.boxscore(game_id="2025020737")
+# away_id = box["awayTeam"]["id"]
+# home_id = box["homeTeam"]["id"]
+
+
+
+# away_abbr = box["awayTeam"]["abbrev"]
+# home_abbr = box["homeTeam"]["abbrev"]
+# print(away_abbr, home_abbr)
+
+# away_roster = client.teams.team_roster(team_abbr=away_abbr, season="20252026")
+# home_roster = client.teams.team_roster(team_abbr=home_abbr, season="20252026")
+
+# # Create away players dataframe
+# away_players = pd.DataFrame(away_roster['forwards'] + away_roster['defensemen'] + away_roster['goalies'])
+# away_players['playerId'] = away_players['id']
+# away_players['playerName'] = away_players['firstName'].apply(lambda x: x['default']) + " " + away_players['lastName'].apply(lambda x: x['default'])
+# away_players['team'] = away_abbr
+# away_players = away_players[['playerId', 'playerName', 'team']]
+
+# # Create home players dataframe
+# home_players = pd.DataFrame(home_roster['forwards'] + home_roster['defensemen'] + home_roster['goalies'])
+# home_players['playerId'] = home_players['id']
+# home_players['playerName'] = home_players['firstName'].apply(lambda x: x['default']) + " " + home_players['lastName'].apply(lambda x: x['default'])
+# home_players['team'] = home_abbr
+# home_players = home_players[['playerId', 'playerName', 'team']]
+
+# # Combine both rosters
+# all_players = pd.concat([away_players, home_players], ignore_index=True)
+
+# # Merge boxscore with player info
+# boxscore = all_players.merge(boxscore, on="playerId", how="left")
+# boxscore = boxscore.fillna(0)
+
+# boxscore.to_csv('boxscore.csv', index=False)
